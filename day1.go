85e4b66f8325300f1b9a63d3fab5ff27c4acd8b9
mohan murali <mohanmuralid28@gmail.com>
2023-12-03 21:23:20 +0000

    completed first task :)

---
+++ day1.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+func main()  {
+	// sum := 0
+	dat, err := os.Open("./data/day1-1.txt")
+	check(err)
+
+	fileScanner := bufio.NewScanner(dat)
+	fileScanner.Split(bufio.ScanLines)
+	sum:= 0
+
+	for fileScanner.Scan(){
+		var numberArray [] int
+		line := fileScanner.Text()
+		for _, c := range line {
+			if unicode.IsDigit(c){
+				num , e := strconv.Atoi(string(c))
+				check(e)
+				numberArray = append(numberArray, num)
+			}
+		}
+		length := len(numberArray)
+		first := numberArray[0]
+		last := numberArray[length -1]
+		fmt.Println(first, last, sum)
+		sum = sum + (first * 10) + last
+	}
+
+	dat.Close()
+
+	fmt.Println(sum)
+}
+
+func check(e error){
+	if e!= nil {
+		panic(e)
+	}
+}
