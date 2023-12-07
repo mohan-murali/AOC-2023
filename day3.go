2af7ecba052763646c54cb97467c40b1f5b03c60
mohan murali <mohanmuralid28@gmail.com>
2023-12-07 16:10:51 +0000

    Changes to get number

--- day3.go
+++ day3.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
+
 func day3_task1(){
-	lines:= readFileToStringArray("./day3.txt")
+	lines:= readFileToStringArray("./data/day3.txt")
+	for _, line := range lines {
+		number := ""
+		digitStarted := false
+		for _, char := range line {
+
+			if unicode.IsDigit(char){
+				digitStarted = true
+				number += string(char)
+			}
+
+			if !unicode.IsDigit(char) && digitStarted {
+				num, err := strconv.Atoi(number)
+				check(err)
+				fmt.Println(num)
+				number = ""
+				digitStarted = false
+			}
+		}
+
+	}
 }
--- main.go
+++ main.go
@@ -4,5 +4,6 @@ func main(){
 	// task1()
 	// task2()
 	// day2_task1()
-	day2_task2()
+	// day2_task2()
+	day3_task1()
 }
