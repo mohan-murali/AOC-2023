fda7aab19873e649744dcea0bf65f7f4e1372a4f
mohan murali <mohanmuralid28@gmail.com>
2023-12-08 16:59:21 +0000

    Day 4 attempt

--- day4.go
+++ day4.go
@@ -7,14 +7,39 @@ import (
 
 func day4_task1(){
 	lines := readFileToStringArray("./data/day4.txt")
+	sum:=0
 
 	//Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 	for _, line:= range lines {
 		game := strings.Split(line, ":")
 		gameNumbers := strings.Split(game[1], "|")
-		lucky, numbers := gameNumbers[0], gameNumbers[1]
+		lucky, numbers := strings.TrimSpace(gameNumbers[0]), strings.TrimSpace(gameNumbers[1])
 		luck := strings.Split(lucky, " ")
 		curr := strings.Split(numbers, " ")
-		fmt.Println(luck, curr)
+		luckyNumbers := make(map[string] bool)
+		for _, num:= range luck{
+			luckyNumbers[num]=false
+		}
+		numbersExist:=false
+		power :=1
+		for _, num:= range curr {
+			_, exists := luckyNumbers[num]
+			if exists {
+				luckyNumbers[num] = true
+				if numbersExist {
+					power *=2
+				}
+				numbersExist = true
+
+			}
+		}
+
+		fmt.Println(power, sum)
+
+		if numbersExist {
+			sum += power
+		}
 	}
+
+	fmt.Println(sum)
 }
