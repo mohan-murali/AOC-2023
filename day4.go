64081f2aa355fe67efbb0c4239dfd37a793b7b08
mohan murali <mohanmuralid28@gmail.com>
2023-12-10 22:29:34 +0000

    Day 4 task 2 attempt 1

--- day4.go
+++ day4.go
@@ -8,11 +8,53 @@ import (
 
 func day4_task2(){
 	lines:= readFileToStringArray("./data/day4.txt")
+	givenGames := make(map[int] int)
 	for _, line:= range lines {
 		game:= strings.Split(line, ":")
-		gameId, gameNumbers := strings.Split(game[0], " ")[1], strings.Split(game[1], "|")
-		fmt.Println(gameId, gameNumbers)
+		gameId := strings.Split(game[0], " ")[1]
+		id,err := strconv.Atoi(gameId)
+		check(err)
+		givenGames[id] = 1
 	}
+
+	for _, line:= range lines {
+		game:= strings.Split(line, ":")
+		gameNumbers := strings.Split(game[1], "|")
+		gameId := strings.Split(game[0], " ")[1]
+		id,err := strconv.Atoi(gameId)
+		check(err)
+		lucky, numbers := strings.TrimSpace(gameNumbers[0]), strings.TrimSpace(gameNumbers[1])
+		luck := strings.Split(lucky, " ")
+		curr := strings.Split(numbers, " ")
+		luckyNumbers := make(map[int] bool)
+		for _, num:= range luck{
+			number, err := strconv.Atoi(num)
+			if(err == nil){
+				luckyNumbers[number]=false
+			}
+		}
+		scartchId := id
+		for _, num:= range curr {
+			number, err:= strconv.Atoi(num)
+			if(err == nil){
+				_, exists := luckyNumbers[number]
+				if exists {
+					luckyNumbers[number] = true
+					scartchId ++
+					givenGames[scartchId] = givenGames[scartchId] + 1
+				}
+			}
+		}
+	}
+
+	sum:= 0
+
+	fmt.Println(givenGames)
+	for _, game := range givenGames {
+		fmt.Println(game)
+	}
+
+	fmt.Println(sum)
 }
 
 func day4_task1(){
